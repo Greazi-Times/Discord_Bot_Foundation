6272b376bd9593100c2177146f7d0f181650db1b
Greazi <mail@greazi.com>
2021-12-22 22:24:07 +0100

    Added Constants class

---
+++ src/main/java/com/greazi/discordbotfoundation/constants/Constants.java
@@ -0,0 +1,22 @@
+package com.greazi.discordbotfoundation.constants;
+
+public final class Constants {
+
+	public static final class File {
+
+		/**
+		 * The name of our settings file
+		 */
+		public static final String SETTINGS = "Settings.json";
+
+		/**
+		 * The error.log file created automatically to log errors to !!THIS FILE WILL BE USED LATER ON BUT IS HERE TO JUST HAVE IT ALREADY!!
+		 */
+		public static final String ERRORS = "error.log";
+
+		/**
+		 * The debug.log file to log debug messages to
+		 */
+		public static final String DEBUG = "debug.log";
+	}
+}
