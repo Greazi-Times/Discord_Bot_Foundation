1bc3eaf074c1077b49b950dee47cc197c8a97623
Greazi <mail@greazi.com>
2021-12-23 21:38:56 +0100

    Temp settings files
    A Temp way for the settings files. As I am still figuring out a good way for the settings files.

--- src/main/java/com/greazi/discordbotfoundation/settings/SimpleSettings.java
+++ src/main/java/com/greazi/discordbotfoundation/settings/SimpleSettings.java
@@ -2,6 +2,7 @@ package com.greazi.discordbotfoundation.settings;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.greazi.discordbotfoundation.constants.Constants;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
@@ -11,110 +12,27 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SimpleSettings {
 
+	private String token;
+	private String activity;
 
-	protected String getSettingsFileName() {
-		return "settings.yml";
+
+	SimpleSettings() {
+
 	}
 
-	private static SimpleSettings instance;
-
-	public static SimpleSettings getInstance(){
-		if(instance == null){
-			instance = new SimpleSettings();
-		}
-
-		return instance;
+	public String getToken() {
+		return token;
 	}
 
-	private JsonObject root;
-
-
-
-	private SimpleSettings() {
-		File file = new File(getSettingsFileName());
-
-		if(!file.exists()){
-			try {
-				InputStream src = SimpleSettings.class.getResourceAsStream("/Settings.json");
-				Files.copy(src, Paths.get(file.toURI()), StandardCopyOption.REPLACE_EXISTING);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		try {
-			String json = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
-
-			JsonParser jsonParser = new JsonParser();
-			root = (JsonObject) jsonParser.parse(json);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public String getActivity() {
+		return activity;
 	}
 
-	public boolean isConfigured(){
-		return !getToken().isEmpty() || !getActivity().isEmpty();
-	}
-
-	public String getToken(){
-		return root.get("token").getAsString();
-	}
-
-	public String getActivity(){
-		return root.get("activity").getAsString();
-	}
-
-	public String getApiToken(){
-		return root.get("apiToken").getAsString();
-	}
-
-	public String getSongodaApiToken(){
-		return root.get("songodaApiToken").getAsString();
-	}
-
-	public String getMySqlHost(){
-		return root.get("mySQL_host").getAsString();
-	}
-
-	public String getMySqlPort(){
-		return root.get("mySQL_port").getAsString();
-	}
-
-	public String getMySqlDatabase(){
-		return root.get("mySQL_database").getAsString();
-	}
-
-	public String getMySqlUsername(){
-		return root.get("mySQL_username").getAsString();
-	}
-
-	public String getMySqlPassword(){
-		return root.get("mySQL_password").getAsString();
-	}
-
-	public String getGithubToken(){
-		return root.get("githubToken").getAsString();
-	}
-
-	public String getPteroUrl(){
-		return root.get("pterodactylUrl").getAsString();
-	}
-
-	public String getPteroClientToken(){
-		return root.get("pterodactylClientToken").getAsString();
-	}
-
-	public String getPteroApiToken(){
-		return root.get("pterodactylApiToken").getAsString();
-	}
-
-	public String getPaypalClientID(){
-		return root.get("PaypalClientID").getAsString();
-	}
-	public String getPaypalClientSecret(){
-		return root.get("PaypalClientID").getAsString();
-	}
 }
--- src/main/java/com/greazi/discordbotfoundation/settings/SimpleYaml.java
+++ src/main/java/com/greazi/discordbotfoundation/settings/SimpleYaml.java
@@ -3,75 +3,23 @@ package com.greazi.discordbotfoundation.settings;
 import com.greazi.discordbotfoundation.constants.Constants;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.Yaml;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 /**
  * SimpleJson is a tool that checks if the settings file is there,
- * As well as making some easy methodes to generate a proper settings file.
+ * As well as making some easy methods to generate a proper settings file.
  */
-public class SimpleJson {
+public class SimpleYaml {
 
-	/**
-	 * Check if the settings file exists.
-	 * @return Boolean
-	 */
-	public static Boolean settingsFileExists() {
-		return new File(Constants.File.SETTINGS).exists();
-	}
-
-	@SuppressWarnings("unchecked")
-	public static void settingsFileCreate() {
-		JSONObject generalSettings = new JSONObject();
-		generalSettings.put("Token", "00000000000000000000000000000000000000000000000000000000000");
-		generalSettings.put("Activity", "For help");
-		generalSettings.put("Settings_Version", "1");
-
-		JSONObject generalObjet = new JSONObject();
-		generalObjet.put("General", generalSettings);
-	}
-
-	public static void settingsFileCheckVersion() {
-
-	}
-
-
-	public static void main( String[] args )
-	{
-		//First Employee
-		JSONObject employeeDetails = new JSONObject();
-		employeeDetails.put("firstName", "Lokesh");
-		employeeDetails.put("lastName", "Gupta");
-		employeeDetails.put("website", "howtodoinjava.com");
-
-		JSONObject employeeObject = new JSONObject();
-		employeeObject.put("employee", employeeDetails);
-
-		//Second Employee
-		JSONObject employeeDetails2 = new JSONObject();
-		employeeDetails2.put("firstName", "Brian");
-		employeeDetails2.put("lastName", "Schultz");
-		employeeDetails2.put("website", "example.com");
-
-		JSONObject employeeObject2 = new JSONObject();
-		employeeObject2.put("employee", employeeDetails2);
-
-		//Add employees to list
-		JSONArray employeeList = new JSONArray();
-		employeeList.add(employeeObject);
-		employeeList.add(employeeObject2);
-
-		//Write JSON file
-		try (FileWriter file = new FileWriter("employees.json")) {
-			//We can write any JSONArray or JSONObject instance to the file
-			file.write(employeeList.toJSONString());
-			file.flush();
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+	Yaml yaml = new Yaml();
+	InputStream inputStream = this.getClass()
+			.getClassLoader()
+			.getResourceAsStream(Constants.File.SETTINGS);
 }
