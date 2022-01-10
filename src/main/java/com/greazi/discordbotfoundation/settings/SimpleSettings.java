ea258844214e2d5171d737f1dcab544b77bd0f94
Greazi <mail@greazi.com>
2022-01-10 19:48:01 +0100

    Added simple settings
    A class that is the foundation system for the settings

--- src/main/java/com/greazi/discordbotfoundation/settings/SimpleSettings.java
+++ src/main/java/com/greazi/discordbotfoundation/settings/SimpleSettings.java
@@ -2,7 +2,7 @@ package com.greazi.discordbotfoundation.settings;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.greazi.discordbotfoundation.constants.Constants;
+import okhttp3.internal.http2.Settings;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
@@ -12,27 +12,53 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class SimpleSettings {
 
-	private String token;
-	private String activity;
+	private static SimpleSettings instance;
 
+	public static SimpleSettings getInstance(){
+		if(instance == null){
+			instance = new SimpleSettings();
+		}
 
-	SimpleSettings() {
-
+		return instance;
 	}
 
-	public String getToken() {
-		return token;
+	private JsonObject root;
+
+	private SimpleSettings() {
+		File file = new File("Settings.json");
+
+		if(!file.exists()){
+			try {
+				InputStream src = Settings.class.getResourceAsStream("/Settings.json");
+				Files.copy(src, Paths.get(file.toURI()), StandardCopyOption.REPLACE_EXISTING);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		try {
+			String json = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+
+			JsonParser jsonParser = new JsonParser();
+			root = (JsonObject) jsonParser.parse(json);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
-	public String getActivity() {
-		return activity;
+	public String getToken(){
+		return root.get("token").getAsString();
+	}
+
+	public String getName(){
+		return root.get("name").getAsString();
+	}
+
+	public String getActivity(){
+		return root.get("activity").getAsString();
 	}
 
 }
