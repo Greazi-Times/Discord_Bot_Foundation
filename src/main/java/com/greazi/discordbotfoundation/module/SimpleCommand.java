58fa743d9c21a5f966b5db719ac343b362bdf3e8
Greazi <mail@greazi.com>
2022-01-10 19:44:52 +0100

    addedd a simple command setter

---
+++ src/main/java/com/greazi/discordbotfoundation/module/SimpleCommand.java
@@ -0,0 +1,38 @@
+package com.greazi.discordbotfoundation.module;
+
+import com.greazi.discordbotfoundation.SimpleBot;
+import com.greazi.discordbotfoundation.objects.Cooldown;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.interactions.commands.privileges.CommandPrivilege;
+
+import java.util.HashMap;
+
+public abstract class SimpleCommand {
+
+    private final HashMap<String, Cooldown> cooldowns = new HashMap<>();
+    protected SimpleBot bot;
+
+    public SimpleCommand(SimpleBot bot) {
+        this.bot = bot;
+    }
+
+    public abstract String setCommand();
+
+    public abstract String setDescription();
+
+    public abstract CommandPrivilege[] setCommandPrivileges();
+
+    public abstract OptionData[] setOptions();
+
+    public abstract int setCooldown();
+
+    public HashMap<String, Cooldown> getCooldowns() {
+        return cooldowns;
+    }
+
+    public abstract void onCommand(TextChannel channel, Member m, SlashCommandEvent e);
+
+}
