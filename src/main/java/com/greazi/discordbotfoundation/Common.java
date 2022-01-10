7b816ea9023db251ee661ad6fcc66775687793a9
Greazi <mail@greazi.com>
2022-01-10 19:44:07 +0100

    added Common file
    This file will hold every basic methode for logging and message sending.

---
+++ src/main/java/com/greazi/discordbotfoundation/Common.java
@@ -0,0 +1,19 @@
+package com.greazi.discordbotfoundation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+/**
+ * This class holds some pre-made log and message send modules
+ */
+public final class Common {
+
+	/**
+	 * Initialize the log module.
+	 */
+	public static final Logger log = LoggerFactory.getLogger(SimpleBot.class);
+
+}
