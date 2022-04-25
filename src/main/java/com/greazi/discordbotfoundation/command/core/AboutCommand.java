bbba8d7408bef494c57b57ffc768bf58d9fcaa40
Greazi_Time <mail@greazi.com>
2022-04-25 21:15:51 +0200

    fix: Fixed about command output

--- src/main/java/com/greazi/discordbotfoundation/command/core/AboutCommand.java
+++ src/main/java/com/greazi/discordbotfoundation/command/core/AboutCommand.java
@@ -7,12 +7,18 @@
 
 package com.greazi.discordbotfoundation.command.core;
 
+import com.greazi.discordbotfoundation.SimpleBot;
+import com.greazi.discordbotfoundation.constants.Constants;
 import com.greazi.discordbotfoundation.handlers.commands.SimpleSlashCommand;
+import com.greazi.discordbotfoundation.settings.SimpleSettings;
 import com.greazi.discordbotfoundation.utils.SimpleEmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
 import java.awt.*;
 
+/**
+ * An about command that displays information about the bot
+ */
 public class AboutCommand extends SimpleSlashCommand {
 
     public AboutCommand() {
@@ -24,9 +30,10 @@ public class AboutCommand extends SimpleSlashCommand {
     @Override
     protected void execute(SlashCommandInteractionEvent event) {
         event.replyEmbeds(new SimpleEmbedBuilder("About the bot")
-                .text("Gateway: " + event.getJDA().getGatewayPing(),
-                        "Rest: " + event.getJDA().getRestPing().complete(),
-                        "Response: " + event.getJDA().getResponseTotal())
+                .text("This bot uses the [Discord Bot Foundation](https://github.com/Greazi-Times/Discord_Bot_Foundation) version: " + Constants.Version.FOUNDATION + "\n\nBot information:")
+                .field("Bot name", SimpleSettings.Bot.Name(), true)
+                .field("Bot version", SimpleBot.getVersion(), true)
+                .field("Developer", SimpleBot.getInstance().getDeveloper(), true)
                 .setColor(Color.decode("#2f3136"))
                 .build()).setEphemeral(true).queue();
     }
