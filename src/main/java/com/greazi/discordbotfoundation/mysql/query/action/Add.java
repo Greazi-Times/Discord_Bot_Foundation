808afa1c3ac054780ff2725552d6d981138308d1
Greazi_Time <mail@greazi.com>
2022-06-08 20:17:15 +0200

    chore: temp fix

--- src/main/java/com/greazi/discordbotfoundation/mysql/query/action/Add.java
+++
@@ -1,9 +0,0 @@
-package com.greazi.discordbotfoundation.mysql.query.action;
-
-public class Add extends Action {
-
-    @Override
-    public String getPrefix() {
-        return "ADD ";
-    }
-}
--- src/main/java/com/greazi/discordbotfoundation/mysql/query/action/Regular.java
+++
@@ -1,9 +0,0 @@
-package com.greazi.discordbotfoundation.mysql.query.action;
-
-public class Regular extends Action {
-
-    @Override
-    public String getPrefix() {
-        return "";
-    }
-}
