99ba9fee81b32c240a96d95271a02c78eeeebaa2
Greazi_Time <mail@greazi.com>
2022-06-08 20:42:28 +0200

    chore: Moved member system to backup

---
+++ Backup/members/MemberStorageEvents.java
@@ -0,0 +1,42 @@
+package com.greazi.discordbotfoundation.managers.members;
+
+import com.greazi.discordbotfoundation.Common;
+import com.greazi.discordbotfoundation.SimpleBot;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberUpdateEvent;
+import net.dv8tion.jda.api.events.guild.member.update.GuildMemberUpdateNicknameEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class MemberStorageEvents extends ListenerAdapter {
+
+    @Override
+    public void onGuildMemberJoin(@NotNull GuildMemberJoinEvent event) {
+        SimpleBot.getMemberStorage().insertMember(event.getMember());
+        super.onGuildMemberJoin(event);
+    }
+
+    @Override
+    public void onGuildMemberRemove(GuildMemberRemoveEvent event) {
+        SimpleBot.getMemberStorage().deleteMember(event.getUser().getId());
+    }
+
+    @Override
+    public void onGuildMemberUpdateNickname(GuildMemberUpdateNicknameEvent event) {
+        Common.log("member updated nickname");
+        SimpleBot.getMemberStorage().updateMember(event.getMember());
+    }
+
+    @Override
+    public void onGuildMemberUpdate(GuildMemberUpdateEvent event) {
+        Common.log("member updated");
+        SimpleBot.getMemberStorage().updateMember(event.getMember());
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        Common.log(event.getMessage().getContentRaw());
+    }
+}
