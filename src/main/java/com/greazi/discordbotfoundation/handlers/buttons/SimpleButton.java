813ea9ac86b5e4b010a8c53f16561906953daaf9
Greazi <mail@greazi.com>
2023-05-10 18:39:57 +0200

    chore: fixed imports

--- src/main/java/com/greazi/discordbotfoundation/handlers/buttons/SimpleButton.java
+++ src/main/java/com/greazi/discordbotfoundation/handlers/buttons/SimpleButton.java
@@ -8,6 +8,7 @@
 package com.greazi.discordbotfoundation.handlers.buttons;
 
 import com.greazi.discordbotfoundation.SimpleBot;
+import com.greazi.discordbotfoundation.utils.SimpleEmbedBuilder;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
