8770035abe26e0b470f5a31df3d475d82515fd55
Greazi-Times <mail@greazi.com>
2024-01-24 17:04:54 +0100

    feat(command): Redo of the ping command

--- src/main/java/com/greazi/discordbotfoundation/command/PingCommand.java
+++ src/main/java/com/greazi/discordbotfoundation/command/PingCommand.java
@@ -7,7 +7,7 @@
 
 package com.greazi.discordbotfoundation.command;
 
-import java.awt.*;
+import com.greazi.discordbotfoundation.Common;
 
 /**
  * A simple ping command to get the latency of the bot
@@ -20,16 +20,16 @@ public class PingCommand extends SimpleCommand {
      */
     public PingCommand() {
         super("ping");
-        setDescription("Get the latency of the bot");
+        setDescription("Get the latency of the bot - 1");
     }
 
     @Override
     protected void onCommand() {
-        event.replyEmbeds(new SimpleEmbedBuilder("Bot latency")
-                .text("Gateway: " + event.getJDA().getGatewayPing(),
-                        "Rest: " + event.getJDA().getRestPing().complete(),
-                        "Response: " + event.getJDA().getResponseTotal())
-                .setColor(Color.decode("#2f3136"))
-                .build()).setEphemeral(true).queue();
+        Common.warning("Ping command executed");
+        replyEmbed("Bot latency",
+                "Gateway: " + event.getJDA().getGatewayPing(),
+                "Rest: " + event.getJDA().getRestPing().complete(),
+                "Response: " + event.getJDA().getResponseTotal()
+        );
     }
 }
